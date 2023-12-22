6f21dd622f91feb848ed7eead5c3f9a97180cfe4
Anastasiia <nastya.kll@yandex.ru>
2023-12-23 02:20:21 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app">
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript 2", body: "Описание поста 2" },
+        { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
+        { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
+      ],
+      title: "",
+      body: "",
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
+  },
+};
+</script>
+
+<!-- атрибут scoped - стили будут доступны только этому компоненту, и не будут доступны извне -->
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-bind:value="title"
+      @input="title = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-bind:value="body"
+      @input="body = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  padding: 10px 15px;
+  border: 1px solid teal;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
