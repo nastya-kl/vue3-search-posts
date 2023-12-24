2184d075699f6dfc1e0110ac6805d6ee10da1abe
Anastasiia <nastya.kll@yandex.ru>
2023-12-24 14:38:49 +0800

    feature: реализовать удаление постов

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="app">
     <post-form @create="createPost" />
-    <post-list :posts="posts" />
+    <post-list
+      :posts="posts"
+      @remove="removePost"
+    />
   </div>
 </template>
 
@@ -27,6 +30,9 @@ export default {
   methods: {
     createPost(post) {
       this.posts.push(post);
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
     },
   },
 };
