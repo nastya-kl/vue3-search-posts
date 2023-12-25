441f6a9322d63c65161f4cd1030adb12e86d4360
Anastasiia <nastya.kll@yandex.ru>
2023-12-25 19:43:23 +0800

    feature: реализовать бесконечную ленту

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,8 @@
       v-if="!isPostsLoading"
     />
     <div v-else>Идёт загрузка...</div>
-    <div class="page__wrapper">
+    <div ref="observer" class="observer"></div>
+    <!-- <div class="page__wrapper">
       <div 
         v-for="pageNumber in totalPages" 
         :key="pageNumber"
@@ -27,7 +28,7 @@
       >
         {{ pageNumber }}
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -71,10 +72,10 @@ export default {
     showDialoge() {
       this.dialogVisible = true;
     },
-    changePage(pageNumber) {
-      this.page = pageNumber;
-      this.fetchPosts();
-    },
+    // changePage(pageNumber) {
+    //   this.page = pageNumber;
+    //   this.fetchPosts();
+    // },
     async fetchPosts() {
       try {
         this.isPostsLoading = true;
@@ -87,7 +88,9 @@ export default {
             },
           }
         );
-        this.totalPages = Math.ceil(response.headers["x-total-count"] / this.limit);
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
+        );
         this.posts = response.data;
       } catch (error) {
         alert("Ошибка");
@@ -95,9 +98,41 @@ export default {
         this.isPostsLoading = false;
       }
     },
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        );
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
+        );
+        this.posts = [...this.posts, ...response.data];
+      } catch (error) {
+        alert("Ошибка");
+      }
+    },
   },
   mounted() {
     this.fetchPosts();
+    console.log(this.$refs.observer);
+    const options = {
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts();
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
@@ -142,6 +177,9 @@ export default {
 }
 .current-page {
   border: 2px solid teal;
-
+}
+.observer {
+  height: 30px;
+  background: green;
 }
 </style>
